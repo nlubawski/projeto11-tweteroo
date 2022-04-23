af71fd74e84ba26baf907b0ed4055853f4e67a75
Nathan Lubawski <nathanlubawski1@gmail.com>
2022-04-23 14:30:14 -0300

    realizado post pra receber pelo body o usuario e avatar, que é salvo no array de usuários, retornada mensagem OK

--- index.js
+++ index.js
@@ -1,7 +1,17 @@
 import express from "express";
-//import cors from "cors";
+import cors from "cors";
 
 const app = express();
+app.use(cors());
+app.use(express.json());
+
+let usuarios = [];
+
+app.post("/sign-up", (req,res) => {
+  usuarios.push(req.body);
+  console.log(usuarios)
+  res.send("OK")
+})
 
 
-app.listen(5000, () => {console.log("teste")});
+app.listen(5000);
